d4176f2877fba17a7d9133abff4ab380a520173e
limesoda314 <amiya017@ucr.edu>
2022-06-24 22:15:20 -0700

    Worked on the basic outline of the website including the hero, body and footer. Will need to adjust the format to include a navbar or menu as well as fine tune placement

---
+++ src/App.svelte
@@ -0,0 +1,38 @@
+<script>
+	import Navbar from "./Navbar.svelte"
+	
+</script>
+<svelte:head>
+
+	<title>CCC@UCR</title>
+
+</svelte:head>
+
+<Navbar/>
+
+
+<body>
+
+	<div class="hero">
+		<div class="center">
+			<h1>CCC at UCR</h1>
+		</div>
+	</div>
+
+	<div class="container"> 
+		<div class="article">
+			<h4>The Competitive Coding Club at UC Riverside</h4>
+			<h4>Events and Meetings</h4>
+			<h4>Socials and Links</h4>
+			
+		</div>
+	</div>
+
+	<div class="footer">
+
+		Visit our instagram and join our slack and discord! 
+	
+	</div>
+
+</body>
+
